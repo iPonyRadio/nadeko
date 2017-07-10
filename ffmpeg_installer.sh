c1dba0fca986830eeaa8707693192cf675c064b0
Pixel Perfect <pixelperfectpon3@gmail.com>
2017-07-10 12:06:29 -0400

    Suppress stdout

--- ffmpeg_installer.sh
+++ ffmpeg_installer.sh
@@ -1,76 +1,86 @@
-yum-config-manager --add-repo http://www.nasm.us/nasm.repo
-yum install autoconf automake bzip2 cmake freetype-devel gcc gcc-c++ git libtool make mercurial nasm pkgconfig zlib-devel -y
-mkdir ./ffmpeg_sources
-cd ./ffmpeg_sources
+yum-config-manager --add-repo http://www.nasm.us/nasm.repo 1>/dev/null
+yum install autoconf automake bzip2 cmake freetype-devel gcc gcc-c++ git libtool make mercurial nasm pkgconfig zlib-devel -y 1>/dev/null
+mkdir ./ffmpeg_sources 1>/dev/null
+cd ./ffmpeg_sources 1>/dev/null
 
-curl -O http://www.tortall.net/projects/yasm/releases/yasm-1.3.0.tar.gz
-tar xzvf yasm-1.3.0.tar.gz
-cd yasm-1.3.0
-./configure --prefix="/opt/ffmpeg_build" --bindir="/opt/bin"
-make
+echo building yasm
+curl -O http://www.tortall.net/projects/yasm/releases/yasm-1.3.0.tar.gz 1>/dev/null
+tar xzvf yasm-1.3.0.tar.gz 1>/dev/null
+cd yasm-1.3.0 1>/dev/null
+./configure --prefix="/opt/ffmpeg_build" --bindir="/opt/bin" 1>/dev/null
+make 1>/dev/null
 make install
 
-cd /opt/ffmpeg_sources
-git clone --depth 1 http://git.videolan.org/git/x264
-cd x264
-PKG_CONFIG_PATH="/opt/ffmpeg_build/lib/pkgconfig" ./configure --prefix="/opt/ffmpeg_build" --bindir="/opt/bin" --enable-static
-make
+echo building x264
+cd /opt/ffmpeg_sources 1>/dev/null
+git clone --depth 1 http://git.videolan.org/git/x264 1>/dev/null
+cd x264 1>/dev/null
+PKG_CONFIG_PATH="/opt/ffmpeg_build/lib/pkgconfig" ./configure --prefix="/opt/ffmpeg_build" --bindir="/opt/bin" --enable-static 1>/dev/null
+make 1>/dev/null
 make install
 
-cd /opt/ffmpeg_sources
-hg clone https://bitbucket.org/multicoreware/x265
-cd /opt/ffmpeg_sources/x265/build/linux
-cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="/opt/ffmpeg_build" -DENABLE_SHARED:bool=off ../../source
-make
+echo building x265
+cd /opt/ffmpeg_sources 1>/dev/null
+hg clone https://bitbucket.org/multicoreware/x265 1>/dev/null
+cd /opt/ffmpeg_sources/x265/build/linux 1>/dev/null
+cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="/opt/ffmpeg_build" -DENABLE_SHARED:bool=off ../../source 1>/dev/null
+make 1>/dev/null
 make install
 
-cd /opt/ffmpeg_sources
-git clone --depth 1 https://github.com/mstorsjo/fdk-aac
-cd fdk-aac
-autoreconf -fiv
-./configure --prefix="/opt/ffmpeg_build" --disable-shared
-make
+echo building fdk-aac
+cd /opt/ffmpeg_sources 1>/dev/null
+git clone --depth 1 https://github.com/mstorsjo/fdk-aac 1>/dev/null
+cd fdk-aac 1>/dev/null
+autoreconf -fiv 1>/dev/null
+./configure --prefix="/opt/ffmpeg_build" --disable-shared 1>/dev/null
+make 1>/dev/null
 make install
 
-cd /opt/ffmpeg_sources
-curl -L -O http://downloads.sourceforge.net/project/lame/lame/3.99/lame-3.99.5.tar.gz
-tar xzvf lame-3.99.5.tar.gz
-cd lame-3.99.5
-./configure --prefix="/opt/ffmpeg_build" --bindir="/opt/bin" --disable-shared --enable-nasm
-make
+echo building lame
+cd /opt/ffmpeg_sources 1>/dev/null
+curl -L -O http://downloads.sourceforge.net/project/lame/lame/3.99/lame-3.99.5.tar.gz 1>/dev/null
+tar xzvf lame-3.99.5.tar.gz 1>/dev/null
+cd lame-3.99.5 1>/dev/null
+./configure --prefix="/opt/ffmpeg_build" --bindir="/opt/bin" --disable-shared --enable-nasm 1>/dev/null
+make 1>/dev/null
 make install
 
-cd /opt/ffmpeg_sources
-curl -O https://archive.mozilla.org/pub/opus/opus-1.1.5.tar.gz
-tar xzvf opus-1.1.5.tar.gz
-cd opus-1.1.5
-./configure --prefix="/opt/ffmpeg_build" --disable-shared
-make
+echo building opus
+cd /opt/ffmpeg_sources 1>/dev/null
+curl -O https://archive.mozilla.org/pub/opus/opus-1.1.5.tar.gz 1>/dev/null
+tar xzvf opus-1.1.5.tar.gz 1>/dev/null
+cd opus-1.1.5 1>/dev/null
+./configure --prefix="/opt/ffmpeg_build" --disable-shared 1>/dev/null
+make 1>/dev/null
 make install
 
-cd /opt/ffmpeg_sources
-curl -O http://downloads.xiph.org/releases/ogg/libogg-1.3.2.tar.gz
-tar xzvf libogg-1.3.2.tar.gz
-cd libogg-1.3.2
-./configure --prefix="/opt/ffmpeg_build" --disable-shared
-make
+echo building libogg
+cd /opt/ffmpeg_sources 1>/dev/null
+curl -O http://downloads.xiph.org/releases/ogg/libogg-1.3.2.tar.gz 1>/dev/null
+tar xzvf libogg-1.3.2.tar.gz 1>/dev/null
+cd libogg-1.3.2 1>/dev/null
+./configure --prefix="/opt/ffmpeg_build" --disable-shared 1>/dev/null
+make 1>/dev/null
 make install
 
-cd /opt/ffmpeg_sources
-curl -O http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.4.tar.gz
-tar xzvf libvorbis-1.3.4.tar.gz
-cd libvorbis-1.3.4
-./configure --prefix="/opt/ffmpeg_build" --with-ogg="/opt/ffmpeg_build" --disable-shared
-make
+echo building libvorbis
+cd /opt/ffmpeg_sources 1>/dev/null
+curl -O http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.4.tar.gz 1>/dev/null
+tar xzvf libvorbis-1.3.4.tar.gz 1>/dev/null
+cd libvorbis-1.3.4 1>/dev/null
+./configure --prefix="/opt/ffmpeg_build" --with-ogg="/opt/ffmpeg_build" --disable-shared 1>/dev/null
+make 1>/dev/null
 make install
 
-cd /opt/ffmpeg_sources
-git clone --depth 1 https://chromium.googlesource.com/webm/libvpx.git
-cd libvpx
-./configure --prefix="/opt/ffmpeg_build" --disable-examples  --as=yasm
-PATH="/opt/bin:$PATH" make
+echo building libvpx
+cd /opt/ffmpeg_sources 1>/dev/null
+git clone --depth 1 https://chromium.googlesource.com/webm/libvpx.git 1>/dev/null
+cd libvpx 1>/dev/null
+./configure --prefix="/opt/ffmpeg_build" --disable-examples  --as=yasm 1>/dev/null
+PATH="/opt/bin:$PATH" make 1>/dev/null
 make install
 
+echo building ffmpeg
 cd /opt/ffmpeg_sources
 curl -O http://ffmpeg.org/releases/ffmpeg-snapshot.tar.bz2
 tar xjvf ffmpeg-snapshot.tar.bz2
