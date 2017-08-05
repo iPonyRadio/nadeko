14fb514a94d481fb45af1140af6da57a80f1149e
Pixel Perfect <pixelperfectpon3@gmail.com>
2017-08-05 01:01:05 -0400

    1.6.1
    From Kwoth/NadekoBot
    3097ef8 Fixed hangman error
    82aac89 animal racing rewritten to be isolated. Please hunt bugs.
    e0be610 .wheel command added (wheel of fortune gambling)
    f3984c8 Nunchi game added. A bit confusing now. Will be polished further tomorrow.
    d9a446d Nunchi is much more fair and forgiving now. There are 5 seconds delays between rounds, and only one player can fail per round (multiple users can still get booted if they're inactive)
    94e4c89 Updated commandlist
    ec7f69f connect4 game added.
    47125ed connect4 bugfixes
    dea9a93 Small nunchi fix, global nadeko improvement

--- nadeko_installer.sh
+++ nadeko_installer.sh
@@ -29,10 +29,10 @@ cd "$root"
 
 echo ""
 echo "Downloading NadekoBot, please wait."
-curl -O https://codeload.github.com/Kwoth/NadekoBot/tar.gz/1.6
-mv 1.6 nadeko.tar.gz
+curl -O https://codeload.github.com/Kwoth/NadekoBot/tar.gz/1.6.1
+mv 1.6.1 nadeko.tar.gz
 tar xvf nadeko.tar.gz
-mv NadekoBot-1.6/ NadekoBot/
+mv NadekoBot-1.6.1/ NadekoBot/
 cd $root/NadekoBot
 echo ""
 echo "NadekoBot downloaded."
